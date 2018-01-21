a97482e9dee990d558b7c1ced631387c1efa0d07
Okz1 <oleg.muhaluna@gmail.com>
2018-01-21 02:13:52 +0200

    deno-4v3

--- demo-4/js/common.js
+++ demo-4/js/common.js
@@ -162,6 +162,12 @@ $(document).ready(function() {
         $('.header-top-nav .close_menu').hide();
     });
 
+var rectangl = $('#rec').offset();
+var paragf = $('.header .slick-corusel p').offset();
+
+if(paragf.top <= 330 && paragf.left <= -448){
+	$("#rec").css('display', "none");
+}
 
 
 });
